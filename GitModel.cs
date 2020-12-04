e7d37ba882808af68e89736ed02022d7aae01d07
69Kostya69 <58574985+69Kostya69@users.noreply.github.com>
2020-12-04 15:52:04 +0200

    Model

---
+++ GitModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTest.Models
+{
+    public class GitModel
+    {
+        public string Login { get; set; }
+        public int Id { get; set; }
+
+        public int Followers { get; set; }
+
+        public int Following { get; set; }
+
+        public int Repo { get; set; }
+        public string DataCreate { get; set; }
+    }
+}
